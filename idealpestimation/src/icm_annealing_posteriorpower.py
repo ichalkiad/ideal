UPD - Major fixes, parallel computation over blocks/rows for likelihoods, testing

idealpestimation/src/icm_annealing_posteriorpower.py
@@ -732,10 +732,10 @@ def icm_posterior_power_annealing(Y, param_positions_dict, args, temperature_rat
     restarts = 0
     estimated_thetas = []
 
-    max_restarts = 2
+    max_restarts = 3
     max_partial_restarts = 5
     max_halving = 2
-    plot_online = False
+    plot_online = True
 
     if plot_online: # and not fastrun: #################################
         # to plot X before it has moved for the first time
@@ -754,7 +754,7 @@ def icm_posterior_power_annealing(Y, param_positions_dict, args, temperature_rat
         mse_z_nonRT_list = []
         xbox = []
         if elementwise:
-            i = 0            
+            i = 0                 
             while i < parameter_space_dim:                                            
                 target_param, vector_index_in_param_matrix, vector_coordinate = get_parameter_name_and_vector_coordinate(param_positions_dict, i=i, d=d)                    
                 theta_test, _ = optimise_posterior_elementwise(target_param, i, vector_index_in_param_matrix, vector_coordinate, 
@@ -762,7 +762,7 @@ def icm_posterior_power_annealing(Y, param_positions_dict, args, temperature_rat
                 theta_curr = theta_test.copy()                    
                 gamma, delta_rate = update_annealing_temperature(gamma, total_iter, temperature_rate, temperature_steps, all_gammas)      
 
-                if plot_online and i in [10, 75, 82, 115, 120, 121]:  ######################################################
+                if plot_online: # and i in [10, 75, 82, 115, 120, 121]:  ######################################################
                     # only compute errors for latter plotting
                     mse_theta_full, mse_x_list, mse_z_list, mse_x_nonRT_list, mse_z_nonRT_list, fig_xz, per_param_ers, per_param_heats, xbox = \
                                 compute_and_plot_mse(theta_true, theta_curr, l, iteration=total_iter, args=args, 
@@ -777,7 +777,7 @@ def icm_posterior_power_annealing(Y, param_positions_dict, args, temperature_rat
                 #     print(i, l)                               
             
             # last entry in mse lists in the same, has been stored twice
-            if plot_online and l==4: ################################################################
+            if plot_online: # and l==4: ################################################################
                 mse_theta_full, mse_x_list, mse_z_list, mse_x_nonRT_list, mse_z_nonRT_list, fig_xz, per_param_ers, per_param_heats, xbox = \
                                 compute_and_plot_mse(theta_true, theta_curr, l, iteration=total_iter+1, args=args, 
                                     param_positions_dict=param_positions_dict, plot_online=plot_online, mse_theta_full=mse_theta_full, 
@@ -785,7 +785,7 @@ def icm_posterior_power_annealing(Y, param_positions_dict, args, temperature_rat
                                     mse_z_nonRT_list=mse_z_nonRT_list, per_param_ers=per_param_ers, 
                                     per_param_heats=per_param_heats, xbox=xbox, plot_restarts=plot_restarts, fastrun=fastrun)
             # plot posteriors during estimation   
-            if plot_online and l==4: #not fastrun:     ########################################################
+            if plot_online: # and l==4: #not fastrun:     ########################################################
                 fig_posteriors, fig_posteriors_annealed, plotting_thetas = plot_posteriors_during_estimation(Y, total_iter, plotting_thetas, 
                                                                                                         theta_curr.copy(), total_iter, fig_posteriors, 
                                                                                                         fig_posteriors_annealed, gamma, 
@@ -927,16 +927,16 @@ def main(J=2, K=2, d=1, total_running_processes=1, data_location="/tmp/",
         for m in range(trialsmin, trialsmax, 1):
             if elementwise:
                 if evaluate_posterior:                    
-                    DIR_out = "{}/{}/estimation_ICM_evaluate_posterior_elementwise_testparallelposterior/".format(data_location, m)
+                    DIR_out = "{}/{}/estimation_ICM_evaluate_posterior_elementwise_May2/".format(data_location, m)
                     # DIR_out = "{}/{}/estimation_ICM_evaluate_posterior_elementwise_gamma1/".format(data_location, m)
                     # DIR_out = "{}/{}/estimation_ICM_evaluate_posterior_elementwise_gamma1_perturb/".format(data_location, m)
                     # DIR_out = "{}/{}/estimation_ICM_evaluate_posterior_elementwise_annealing/".format(data_location, m)
                     # DIR_out = "{}/{}/estimation_ICM_evaluate_posterior_elementwise_annealing_perturb/".format(data_location, m)
                 else:
-                    DIR_out = "{}/{}/estimation_ICM_differentiate_posterior_elementwise_aplhabeta_idxonly_fullplots/".format(data_location, m)
+                    DIR_out = "{}/{}/estimation_ICM_differentiate_posterior_elementwise_May2/".format(data_location, m)
             else:
                 if evaluate_posterior:
-                    DIR_out = "{}/{}/estimation_ICM_evaluate_posterior_vector_test/".format(data_location, m)
+                    DIR_out = "{}/{}/estimation_ICM_evaluate_posterior_vector_May2/".format(data_location, m)
                     # DIR_out = "{}/{}/estimation_ICM_evaluate_posterior_vector_gamma1/".format(data_location, m)
                     # DIR_out = "{}/{}/estimation_ICM_evaluate_posterior_vector_gamma1_perturb/".format(data_location, m)
                     # DIR_out = "{}/{}/estimation_ICM_evaluate_posterior_vector_annealing/".format(data_location, m)
@@ -1036,12 +1036,12 @@ if __name__ == "__main__":
     # before halving annealing rate
     percentage_parameter_change = 0.2
 
-    if not parallel:
-        jax.default_device = jax.devices("cpu")[0]
-        jax.config.update("jax_traceback_filtering", "off")
+    # if not parallel:
+    #     jax.default_device = jax.devices("cpu")[0]
+    #     jax.config.update("jax_traceback_filtering", "off")
     optimisation_method = "L-BFGS-B"
     dst_func = lambda x, y: np.sum((x-y)**2)
-    niter = 50
+    niter = 30
     penalty_weight_Z = 0.0
     constant_Z = 0.0
     retries = 20
@@ -1078,7 +1078,7 @@ if __name__ == "__main__":
     # temperature_steps = [0, 1, 2, 5, 10]
     # temperature_rate = [0.1, 0.2, 0.5, 1] 
 
-    fastrun = True
+    fastrun = False
     max_signal2noise_ratio = 25 # in dB   # max snr
 
     min_sigma_e = (K*prior_scale_x[0, 0] + J*prior_scale_z[0, 0] + J*prior_scale_alpha + K*prior_scale_beta)/((K*J)*(10**(max_signal2noise_ratio/10)))
@@ -1087,8 +1087,8 @@ if __name__ == "__main__":
     tol = 1e-6    
     #/home/ioannischalkiadakis/ideal
     # data_location = "./idealpestimation/data_K{}_J{}_sigmae{}_goodsnr/".format(K, J, str(sigma_e_true).replace(".", ""))
-    # data_location = "/mnt/hdd2/ioannischalkiadakis/idealdata_plotstest/data_K{}_J{}_sigmae{}/".format(K, J, str(sigma_e_true).replace(".", ""))
     data_location = "/mnt/hdd2/ioannischalkiadakis/idealdata_plotstest/data_K{}_J{}_sigmae{}/".format(K, J, str(sigma_e_true).replace(".", ""))
+    # data_location = "/mnt/hdd2/ioannischalkiadakis/idealdata_testmle/data_K{}_J{}_sigmae{}/".format(K, J, str(sigma_e_true).replace(".", ""))
     total_running_processes = 30      
 
     # with open("/mnt/hdd2/ioannischalkiadakis/idealdata_testmle/data_K10000_J100_sigmae0001/0/Utilities.pickle", "rb") as f:

idealpestimation/src/mle.py
@@ -23,7 +23,7 @@ from idealpestimation.src.utils import params2optimisation_dict, \
                                                                     negative_loglik_coordwise, negative_loglik_coordwise_jax, collect_mle_results, \
                                                                         collect_mle_results_batchsize_analysis, sample_theta_curr_init,\
                                                                         get_parameter_name_and_vector_coordinate, check_convergence, rank_and_return_best_theta,\
-                                                                        negative_loglik_coordwise_parallel, plot_loglik_runtimes
+                                                                        negative_loglik_coordwise_parallel, plot_loglik_runtimes, parse_timedelta_string
 
 
 from idealpestimation.src.icm_annealing_posteriorpower import get_evaluation_grid
@@ -40,7 +40,8 @@ def optimise_negativeloglik_elementwise(param, idx, vector_index_in_param_matrix
     niter_minimize = None
     theta_test_in = theta_curr.copy()
     f = lambda x: negative_loglik_coordwise(x, idx, theta_test_in, Y, J, N, d, 
-                                            parameter_names, dst_func, param_positions_dict, penalty_weight_Z, constant_Z)
+                                            parameter_names, dst_func, param_positions_dict, 
+                                            penalty_weight_Z, constant_Z)
     if parallel:
         f_jax = None
     else:    
@@ -324,12 +325,13 @@ def estimate_mle(args):
                 variance_hat = optimisation_dict2paramvectors(variance_local_vec, param_positions_dict_theta, J, N, d, parameter_names) 
                 grid_and_optim_outcome = dict()
                 grid_and_optim_outcome["PID"] = current_pid
-                grid_and_optim_outcome["timestamp"] = time.strftime("%Y-%m-%d %H:%M:%S")
+                grid_and_optim_outcome["timestamp"] = time.strftime("%Y-%m-%d %H:%M:%S")                
                 elapsedtime = timedelta(seconds=time.time()-t0)            
-                total_seconds = int(elapsedtime.total_seconds())
-                hours = total_seconds // 3600
-                minutes = (total_seconds % 3600) // 60
-                seconds = total_seconds % 60            
+                time_obj, hours, minutes, seconds, microsec = parse_timedelta_string(elapsedtime)
+                # total_seconds = int(elapsedtime.total_seconds())
+                # hours = total_seconds // 3600
+                # minutes = (total_seconds % 3600) // 60
+                # seconds = total_seconds % 60            
                 grid_and_optim_outcome["elapsedtime"] = f"{hours}:{minutes:02d}:{seconds:02d}"       
                 grid_and_optim_outcome["elapsedtime_hours"] = hours
                 grid_and_optim_outcome["retry"] = retry
@@ -364,10 +366,11 @@ def estimate_mle(args):
                 grid_and_optim_outcome["PID"] = current_pid
                 grid_and_optim_outcome["timestamp"] = time.strftime("%Y-%m-%d %H:%M:%S")
                 elapsedtime = timedelta(seconds=time.time()-t0)            
-                total_seconds = int(elapsedtime.total_seconds())
-                hours = total_seconds // 3600
-                minutes = (total_seconds % 3600) // 60
-                seconds = total_seconds % 60            
+                time_obj, hours, minutes, seconds, microsec = parse_timedelta_string(elapsedtime)
+                # total_seconds = int(elapsedtime.total_seconds())
+                # hours = total_seconds // 3600
+                # minutes = (total_seconds % 3600) // 60
+                # seconds = total_seconds % 60                 
                 grid_and_optim_outcome["elapsedtime"] = f"{hours}:{minutes:02d}:{seconds:02d}"       
                 grid_and_optim_outcome["elapsedtime_hours"] = hours
                 grid_and_optim_outcome["retry"] = retry
@@ -410,10 +413,11 @@ def estimate_mle(args):
         grid_and_optim_outcome["PID"] = current_pid
         grid_and_optim_outcome["timestamp"] = time.strftime("%Y-%m-%d %H:%M:%S")
         elapsedtime = timedelta(seconds=time.time()-t0)            
-        total_seconds = int(elapsedtime.total_seconds())
-        hours = total_seconds // 3600
-        minutes = (total_seconds % 3600) // 60
-        seconds = total_seconds % 60            
+        time_obj, hours, minutes, seconds, microsec = parse_timedelta_string(elapsedtime)
+        # total_seconds = int(elapsedtime.total_seconds())
+        # hours = total_seconds // 3600
+        # minutes = (total_seconds % 3600) // 60
+        # seconds = total_seconds % 60               
         grid_and_optim_outcome["elapsedtime"] = f"{hours}:{minutes:02d}:{seconds:02d}"       
         grid_and_optim_outcome["elapsedtime_hours"] = hours
         grid_and_optim_outcome["retry"] = retry
@@ -448,10 +452,11 @@ def estimate_mle(args):
         grid_and_optim_outcome["PID"] = current_pid
         grid_and_optim_outcome["timestamp"] = time.strftime("%Y-%m-%d %H:%M:%S")
         elapsedtime = timedelta(seconds=time.time()-t0)            
-        total_seconds = int(elapsedtime.total_seconds())
-        hours = total_seconds // 3600
-        minutes = (total_seconds % 3600) // 60
-        seconds = total_seconds % 60            
+        time_obj, hours, minutes, seconds, microsec = parse_timedelta_string(elapsedtime)
+        # total_seconds = int(elapsedtime.total_seconds())
+        # hours = total_seconds // 3600
+        # minutes = (total_seconds % 3600) // 60
+        # seconds = total_seconds % 60                   
         grid_and_optim_outcome["elapsedtime"] = f"{hours}:{minutes:02d}:{seconds:02d}"       
         grid_and_optim_outcome["elapsedtime_hours"] = hours
         grid_and_optim_outcome["retry"] = retry
@@ -497,9 +502,15 @@ def estimate_mle(args):
     grid_and_optim_outcome = dict()
     grid_and_optim_outcome["PID"] = current_pid
     grid_and_optim_outcome["timestamp"] = timestamp
-    elapsedtime = timedelta(seconds=time.time()-t0)            
-    grid_and_optim_outcome["elapsedtime"] = eltime
-    grid_and_optim_outcome["elapsedtime_hours"] = hours
+    elapsedtime = timedelta(seconds=time.time()-t0)
+    time_obj, hours, minutes, seconds, microsec = parse_timedelta_string(elapsedtime)
+    # total_seconds = int(elapsedtime.total_seconds())
+    # hours = total_seconds // 3600
+    # minutes = (total_seconds % 3600) // 60
+    # seconds = total_seconds % 60                      
+    grid_and_optim_outcome["elapsedtime_besttheta"] = eltime
+    grid_and_optim_outcome["elapsedtime_full"] = f"{hours}:{minutes:02d}:{seconds:02d}"       
+    grid_and_optim_outcome["elapsedtime_full_hours"] = hours
     grid_and_optim_outcome["retry"] = retry
     grid_and_optim_outcome["parameter names"] = parameter_names
     grid_and_optim_outcome["local theta"] = [best_theta.tolist()]
@@ -683,13 +694,13 @@ if __name__ == "__main__":
         sigma_e_true = args.sigmae
 
     print(parallel, Mmin, M, K, J, sigma_e_true, total_running_processes)
-    if not parallel:
-        try:
-            jax.default_device = jax.devices("gpu")[0]
-        except:
-            print("Using cpu")
-            jax.default_device = jax.devices("cpu")[0]
-        jax.config.update("jax_traceback_filtering", "off")
+    # if not parallel:
+    #     try:
+    #         jax.default_device = jax.devices("gpu")[0]
+    #     except:
+    #         print("Using cpu")
+    #         jax.default_device = jax.devices("cpu")[0]
+    #     jax.config.update("jax_traceback_filtering", "off")
 
     optimisation_method = "L-BFGS-B"
     dst_func = lambda x, y: np.sum((x-y)**2)
@@ -697,7 +708,7 @@ if __name__ == "__main__":
     penalty_weight_Z = 0.0
     constant_Z = 0.0
     retries = 30
-    batchsize = 320
+    batchsize = 13
     # In parameter names keep the order fixed as is
     # full, with status quo
     # parameter_names = ["X", "Z", "Phi", "alpha", "beta", "gamma", "delta", "sigma_e"]
@@ -721,7 +732,7 @@ if __name__ == "__main__":
     prior_scale_delta= 1        
     prior_loc_sigmae = 3
     prior_scale_sigmae = 0.5
-    data_location = "/mnt/hdd2/ioannischalkiadakis/idealdata_mmtest_batchsize/data_K{}_J{}_sigmae{}/".format(K, J, str(sigma_e_true).replace(".", ""))
+    data_location = "/mnt/hdd2/ioannischalkiadakis/idealdata_plotstest/data_K{}_J{}_sigmae{}/".format(K, J, str(sigma_e_true).replace(".", ""))
     # data_location = "/mnt/hdd2/ioannischalkiadakis/data_K{}_J{}_sigmae{}_goodsnr/".format(K, J, str(sigma_e_true).replace(".", ""))           
     # with jsonlines.open("{}/synthetic_gen_parameters.jsonl".format(data_location), mode="r") as f:
     #     for result in f.iter(type=dict, skip_invalid=True):                              