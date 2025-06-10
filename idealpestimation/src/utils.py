44f59c06541eadcad50693ec601163e59e209006
ichalkiad <ichalkiad@protonmail.ch>
2025-06-10 08:38:11 +0000

    Args fix

--- idealpestimation/src/utils.py
+++ idealpestimation/src/utils.py
@@ -2092,7 +2092,7 @@ def sample_theta_curr_init(parameter_space_dim, base2exponent, param_positions_d
                 constant_Z, retries, parallel, min_sigma_e, prior_loc_x, prior_scale_x, prior_loc_z,\
                     prior_scale_z, prior_loc_phi, prior_scale_phi, prior_loc_beta, prior_scale_beta, prior_loc_alpha,\
                         prior_scale_alpha, prior_loc_gamma, prior_scale_gamma, prior_loc_delta, prior_scale_delta,\
-                            prior_loc_sigmae, prior_scale_sigmae, _, rng, batchsize = args
+                            prior_loc_sigmae, prior_scale_sigmae, _, rng, batchsize, theta_true = args
 
     if samples_list is None and parameter_space_dim <= 21201:
         sampler = qmc.Sobol(d=parameter_space_dim, scramble=False)   
