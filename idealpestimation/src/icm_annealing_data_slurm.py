78444fee2153456714f89983a0d2e5f0b9f01b51
ichalkiad <ichalkiad@protonmail.ch>
2025-06-10 09:50:00 +0000

    Path test fix

--- idealpestimation/src/icm_annealing_data_slurm.py
+++ idealpestimation/src/icm_annealing_data_slurm.py
@@ -220,7 +220,7 @@ if __name__ == "__main__":
 
     dataspace = "/linkhome/rech/genpuz01/umi36fq/idealdata_slurm_test/"     
     parameter_vector_idx = 0 #int(os.environ["SLURM_ARRAY_TASK_ID"])    
-    parameter_grid = pd.read_csv("/linkhome/rech/genpuz01/umi36fq/slurm_experimentI_icm_data.csv", header=None)
+    parameter_grid = pd.read_csv("/linkhome/rech/genpuz01/umi36fq/slurm_experimentI_icm_data_test.csv", header=None)
     parameter_vector = parameter_grid.iloc[parameter_vector_idx].values
 
     Mmin = int(parameter_vector[0])
