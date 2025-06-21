MInor fix in svd for rotation error

idealpestimation/src/utils.py
@@ -2473,16 +2473,26 @@ def get_min_achievable_mse_under_rotation_trnsl(param_true, param_hat, seedint):
     H = X_centered.T @ Y_centered
     
     # SVD
-    try:
-        U, S, Vt = np.linalg.svd(H)
-    except:
+    if param_true.shape[0] <= 10000:
+        try:
+            U, S, Vt = np.linalg.svd(H)
+        except:
+            svd_out = ca_svd.compute_svd(
+                X=H,
+                n_iter=2,
+                n_components=2,
+                random_state=seedint,
+                engine='sklearn',
+            )
+            U, S, Vt = svd_out.U, svd_out.s, svd_out.V
+    else:
         svd_out = ca_svd.compute_svd(
-            X=H,
-            n_iter=2,
-            n_components=2,
-            random_state=seedint,
-            engine='sklearn',
-        )
+                X=H,
+                n_iter=2,
+                n_components=2,
+                random_state=seedint,
+                engine='sklearn',
+            )
         U, S, Vt = svd_out.U, svd_out.s, svd_out.V
 
     # Construct the rotation matrix