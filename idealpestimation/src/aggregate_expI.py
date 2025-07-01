b0f8036cab535a1fde1da4c92c69eabd03922b65
ichalkiad <ichalkiad@protonmail.ch>
2025-07-01 08:39:24 +0000

    Path fix

--- idealpestimation/src/aggregate_expI.py
+++ idealpestimation/src/aggregate_expI.py
@@ -25,9 +25,9 @@ if __name__ == "__main__":
     batchsize = 1504
     d = 2
     parameter_names = ["X", "Z", "alpha", "beta", "gamma", "sigma_e"]
-    dataspace = "/linkhome/rech/genpuz01/umi36fq/idealdata_rsspaper/" # /mnt/hdd2/ioannischalkiadakis/       
+    dataspace = "/linkhome/rech/genpuz01/umi36fq/" # /mnt/hdd2/ioannischalkiadakis/       
     dir_in = "{}/idealdata_rsspaper/".format(dataspace)
-    dir_out = "{}/ioannischalkiadakis/rsspaper_expI/".format(dataspace)
+    dir_out = "{}/rsspaper_expI/".format(dataspace)
     pathlib.Path(dir_out).mkdir(parents=True, exist_ok=True) 
 
     ipdb.set_trace() 
