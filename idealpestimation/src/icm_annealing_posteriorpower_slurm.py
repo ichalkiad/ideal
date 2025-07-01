c1786dc482e9f8d5ecb57f97dd9f77f0b3c9e65a
ichalkiad <ichalkiad@protonmail.ch>
2025-07-01 06:39:37 +0000

    run icmp

--- idealpestimation/src/icm_annealing_posteriorpower_slurm.py
+++ idealpestimation/src/icm_annealing_posteriorpower_slurm.py
@@ -198,8 +198,8 @@ if __name__ == "__main__":
 
     niter = 20 # 15
     fastrun = True
-    max_restarts = 2
-    max_partial_restarts = 4 #2
+    max_restarts = 3
+    max_partial_restarts = 2 #2
     max_halving = 2
     plot_online = False
     max_signal2noise_ratio = 25 # in dB   # max snr
