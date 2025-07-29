2d1860eda3d4d1df702b0dcac878a82de5c13b0c
ichalkiad <ichalkiad@protonmail.ch>
2025-07-29 05:58:13 +0000

    expiii runs

--- idealpestimation/src/utils.py
+++ idealpestimation/src/utils.py
@@ -2393,7 +2393,7 @@ def sample_theta_curr_init(parameter_space_dim, base2exponent, param_positions_d
         if d > 2:
             raise NotImplementedError("In {}-dimensional space for the ideal points, find a way to generate random initial solutions.")
         sampler = qmc.Sobol(d=21201, scramble=False)    
-        base2exponent = 20 #15
+        base2exponent = 15
         samples = sampler.random_base2(m=base2exponent)
         samplesreshape = samples[1:,:].reshape((-1,1))
         samplesselect = rng.choice(samplesreshape, base2exponent*parameter_space_dim, replace=False)
@@ -3120,11 +3120,11 @@ def get_evaluation_grid(param, vector_coordinate, args, gridpoints_num_plot=None
     if not elementwise:
         gridpoints_num_alpha_beta = gridpoints_num**2
     else:
-        gridpoints_num_alpha_beta = gridpoints_num*5 #2
+        gridpoints_num_alpha_beta = gridpoints_num*6 #2
         if param in ["gamma"]:
-            gridpoints_num_alpha_beta = gridpoints_num*4 # *3 enough for most to converge, then 4 ok - for ICM-P, 5,6 for icm-d
+            gridpoints_num_alpha_beta = gridpoints_num*5 # *3 enough for most to converge, then 4 ok - for ICM-P, 5,6 for icm-d
         if param in ["sigma_e"]: # for ICM-D
-            gridpoints_num_alpha_beta = gridpoints_num*4  # 3 ok for most, then 4 , 6 for icm-d
+            gridpoints_num_alpha_beta = gridpoints_num*5  # 3 ok for most, then 4 , 6 for icm-d
     xx_ = None
 
     if param == "alpha":
