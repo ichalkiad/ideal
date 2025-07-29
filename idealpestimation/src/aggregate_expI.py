d98fe7e79acefc8b1ec62546fdadc1368c883c5a
ichalkiad <ichalkiad@protonmail.ch>
2025-07-29 11:16:40 +0000

    Full run expi aggr

--- idealpestimation/src/aggregate_expI.py
+++ idealpestimation/src/aggregate_expI.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     dir_out = "{}/rsspaper_expI/".format(dataspace)
     pathlib.Path(dir_out).mkdir(parents=True, exist_ok=True) 
 
-    algorithms = ["icmd"] #["icmp", "icmd", "ca", "mle"]
+    algorithms = ["icmp", "icmd", "ca", "mle"]
     colors = {"mle":"Crimson", "ca":"Tomato", "icmd":"ForestGreen", "icmp":"Maroon"}
     for K in Ks:
         for J in Js:
