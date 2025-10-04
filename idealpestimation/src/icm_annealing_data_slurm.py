41209a7efc8a1aa468231aba9ac986e95a7fe7c5
ichalkiad <ichalkiad@protonmail.ch>
2025-10-04 07:05:42 +0000

    icmd expi

--- idealpestimation/src/icm_annealing_data_slurm.py
+++ idealpestimation/src/icm_annealing_data_slurm.py
@@ -262,10 +262,10 @@ if __name__ == "__main__":
     gridpoints_num = dict()
     gridpoints_num["X"] = 80
     gridpoints_num["Z"] = 80
-    gridpoints_num["alpha"] = 150
-    gridpoints_num["beta"] = 150
-    gridpoints_num["gamma"] = 300
-    gridpoints_num["sigma_e"] = 300
+    gridpoints_num["alpha"] = 100 #150
+    gridpoints_num["beta"] = 100 # 150
+    gridpoints_num["gamma"] = 250 # 300
+    gridpoints_num["sigma_e"] = 250 #300
     prior_loc_x = np.zeros((d,))
     prior_scale_x = np.eye(d)
     prior_loc_z = np.zeros((d,))
@@ -289,7 +289,7 @@ if __name__ == "__main__":
 
     niter = 300 #100, 300
     max_restarts = 5 # 2,3 ,5
-    max_partial_restarts = 12 #4 ,5, 10
+    max_partial_restarts = 4 #4 ,5, 10, 12
     max_halving = 2 # not applicable in data annealing
     max_signal2noise_ratio = 25 # in dB   # max snr
 
